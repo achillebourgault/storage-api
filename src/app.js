a617929bedaba64ec8dc372370737d9b5046f087
Achille <achille.bourgault@epitech.eu>
2023-12-28 22:42:14 +0100

    Remove CORS Errors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const apiConfig = require('../api.config');
 const {err} = require("./utils/term");
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 if (checkConfigurationIntegrity(apiConfig)) {
     const apiRoutes = require(`./api/${apiConfig.apiVersion}/api`);
